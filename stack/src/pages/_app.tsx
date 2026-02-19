f51816c7607b0c68852a2e452bbeec8345132a44
santoshmodekruthi <santoshmodekruthi4@gmail.com>
2026-02-19 12:26:14 +0530

    Add routing fix for GitHub Pages 404 redirects

--- stack/src/pages/_app.tsx
+++ stack/src/pages/_app.tsx
@@ -3,7 +3,19 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "@/lib/AuthContext";
 import Head from "next/head";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const { search } = window.location;
+      if (search.startsWith('?/')) {
+        const path = search.slice(2).split('&')[0]; // get the path part
+        window.history.replaceState(null, '', path);
+      }
+    }
+  }, []);
+
   return (
     <>
       <Head>
